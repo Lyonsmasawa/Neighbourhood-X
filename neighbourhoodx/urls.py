3ef72d1ec718e2eb3fc36e164a09a0b152b3b5ff
Lyonsmasawa <lyonsmasawa@gmail.com>
2022-04-13 06:18:06 -0700

    media and static files setup

--- neighbourhoodx/urls.py
+++ neighbourhoodx/urls.py
@@ -1,6 +1,11 @@
 from django.urls import path
 from . import views
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     path('', views.home, name='home'),
 ]
+
+urlpatterns+= static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+urlpatterns+= static(settings.STATIC_URL, document_root=settings.STATIC_ROOT) 
