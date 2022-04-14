e7f00c0bb375d2cfa3a71e90e09a10ff3b789f9e
Lyonsmasawa <lyonsmasawa@gmail.com>
2022-04-14 12:29:53 -0700

    integrate mapbox

--- neighbourhoodx/admin.py
+++ neighbourhoodx/admin.py
@@ -1,10 +1,11 @@
 from django.contrib import admin
 from .models import Administrator, Business, Member, Neighbourhood, Post, SocialServices
+from mapbox_location_field.admin import MapAdmin
 
 # Register your models here.
 admin.site.register(Administrator)
-admin.site.register(Neighbourhood)
-admin.site.register(Member)
-admin.site.register(Business)
+admin.site.register(Neighbourhood, MapAdmin)
+admin.site.register(Member, MapAdmin)
+admin.site.register(Business, MapAdmin)
 admin.site.register(Post)
-admin.site.register(SocialServices)
+admin.site.register(SocialServices, MapAdmin)
