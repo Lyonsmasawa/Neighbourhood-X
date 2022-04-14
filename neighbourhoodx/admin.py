84e3f22bf6b5dc4a947acbdadd2e2b826cae102b
Lyonsmasawa <lyonsmasawa@gmail.com>
2022-04-14 11:12:29 -0700

    register models

--- neighbourhoodx/admin.py
+++ neighbourhoodx/admin.py
@@ -1,6 +1,10 @@
 from django.contrib import admin
-from .models import Administrator, Neighbourhood
+from .models import Administrator, Business, Member, Neighbourhood, Post, SocialServices
 
 # Register your models here.
 admin.site.register(Administrator)
 admin.site.register(Neighbourhood)
+admin.site.register(Member)
+admin.site.register(Business)
+admin.site.register(Post)
+admin.site.register(SocialServices)
