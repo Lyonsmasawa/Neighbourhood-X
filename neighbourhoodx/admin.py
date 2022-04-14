6950da9e430255031859560cd66dbb0405a29be3
Lyonsmasawa <lyonsmasawa@gmail.com>
2022-04-14 10:35:02 -0700

    Neighbourhood model

--- neighbourhoodx/admin.py
+++ neighbourhoodx/admin.py
@@ -1,5 +1,6 @@
 from django.contrib import admin
-from .models import Administrator
+from .models import Administrator, Neighbourhood
 
 # Register your models here.
 admin.site.register(Administrator)
+admin.site.register(Neighbourhood)
--- neighbourhoodx/models.py
+++ neighbourhoodx/models.py
@@ -1,5 +1,3 @@
-from distutils.command.upload import upload
-from pyexpat import model
 from django.db import models
 from django.contrib.auth.models import User
 
@@ -20,3 +18,22 @@ class Administrator(models.Model):
     def __str__(self):
         """Unicode representation of Administrator."""
         return str(self.user.username)
+
+class Neighbourhood(models.Model):
+    """Model definition for Neighbourhood."""
+
+    # TODO: Define fields here
+    admin = models.ForeignKey(Administrator, on_delete=models.CASCADE)
+    name = models.CharField(max_length=30)
+    location = models.CharField(max_length=30)
+    occupants = models.IntegerField(default=1)
+
+    class Meta:
+        """Meta definition for Neighbourhood."""
+
+        verbose_name = 'Neighbourhood'
+        verbose_name_plural = 'Neighbourhoods'
+
+    def __str__(self):
+        """Unicode representation of Neighbourhood."""
+        pass
