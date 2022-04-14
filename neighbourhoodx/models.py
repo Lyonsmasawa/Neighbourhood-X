7823808314fd990f8727b9c1a2cc8c2d9a1d9fc6
Lyonsmasawa <lyonsmasawa@gmail.com>
2022-04-14 10:03:41 -0700

    create administrator model

--- neighbourhoodx/models.py
+++ neighbourhoodx/models.py
@@ -1,3 +1,22 @@
+from distutils.command.upload import upload
+from pyexpat import model
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
+class Administrator(models.Model):
+    """Model definition for Administrator."""
+
+    # TODO: Define fields here
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    profile_photo = models.ImageField(upload_to = 'images')
+
+    class Meta:
+        """Meta definition for Administrator."""
+
+        verbose_name = 'Administrator'
+        verbose_name_plural = 'Administrators'
+
+    def __str__(self):
+        """Unicode representation of Administrator."""
+        return str(self.user.username)
