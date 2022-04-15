5f92386781789a681e15725d709a8027936a5ab0
Lyonsmasawa <lyonsmasawa@gmail.com>
2022-04-15 02:58:39 -0700

    add delete

--- neighbourhoodx/urls.py
+++ neighbourhoodx/urls.py
@@ -16,6 +16,7 @@ urlpatterns = [
     path('social-services/', views.socialServices, name="social-services"),
     path('post/', views.post, name="post"),
     path('adjust/', views.adjust, name="adjust"),
+    path('delete/<str:pk>/', views.delete, name="delete"),
 ]
 
 urlpatterns+= static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
