4deaec31456d5000e84b2bb456473c7f9dae3b86
Lyonsmasawa <lyonsmasawa@gmail.com>
2022-04-15 00:52:25 -0700

    add resident form

--- neighbourhoodx/forms.py
+++ neighbourhoodx/forms.py
@@ -27,3 +27,11 @@ class NeighbourhoodForm(forms.ModelForm):
         model = Neighbourhood
         fields = '__all__'
         exclude = ['admin', 'occupants']
+
+class AddResidentForm(forms.Form):
+    name = forms.CharField(label='Resident name', max_length=50)
+    username = forms.CharField(label='Username', max_length=50)
+    email = forms.EmailField()
+
+
+
